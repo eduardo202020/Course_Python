ccfac69cec48c3af3b1178e24ae27f68458895ac
Eduardo <jguevaral@uni.pe>
2024-01-15 09:03:37 -0500

    Add inicio.py and test.ipynb files with initial code

---
+++ test.py
@@ -0,0 +1,40 @@
+# %%
+
+
+# %%
+nombre = "jose"
+
+print("hola " + nombre + " bienvenido a python")
+
+# %% [markdown]
+# # Este es un titulo para mi codigo 
+# ## ESTE ES UN SUBTITULO
+# ### ESTE ES UN SUBTITULO MAS PEQUEÑO
+# 
+
+# %% [markdown]
+# 
+
+# %%
+# Imprimir un mensaje en la consola
+print("Hola, bienvenido a Python!")
+
+# Realizar una operación matemática
+a = 5
+b = 3
+suma = a + b
+print("La suma de", a, "y", b, "es igual a", suma)
+
+# Definir una función
+def cuadrado(numero):
+    return numero ** 2
+
+# Llamar a la función y mostrar el resultado
+resultado = cuadrado(4)
+print("El cuadrado de 4 es", resultado)
+
+
+# %% [markdown]
+# 
+
+
